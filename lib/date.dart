730d42022980434f3765c1ab4b76331abbd3be3f
Kimdayoung51 <dayong414@gmail.com>
2021-12-06 09:57:15 +0900

    push

--- lib/date.dart
+++ lib/date.dart
@@ -28,26 +28,38 @@ class DatePageState extends State<DatePage>{
          padding: const EdgeInsets.all(10.0),
          itemCount: shows_sort.length,
          itemBuilder: (BuildContext context, int index){
-           return Container(
-             height: 80,
-             child: Row(
-               children: [
-                 SizedBox(width: 10),
-                 Text(shows_sort[index].stdate + " ~"),
-                 SizedBox(width: 20),
-                 SizedBox(
-                   width: 250,
-                   child: Column(
-                     mainAxisAlignment: MainAxisAlignment.center,
-                     crossAxisAlignment: CrossAxisAlignment.start,
-                     children: [
-                       Text("공연명: " + shows_sort[index].show_name),
-                       SizedBox(height: 8),
-                       Text("출연진: " + shows_sort[index].actors.toString()),
-                     ],
+           return Padding(
+             padding: const EdgeInsets.all(8.0),
+             child: Container(
+               height: 80,
+               decoration: BoxDecoration(
+                 borderRadius: BorderRadius.all(
+                   Radius.circular(10),
+                 ),
+                 border: Border.all(
+                   color: Color(0xFFA5E1F2),
+                   width: 2,
+                 ),
+               ),
+               child: Row(
+                 children: [
+                   SizedBox(width: 10),
+                   Text(shows_sort[index].stdate + " ~"),
+                   SizedBox(width: 20),
+                   SizedBox(
+                     width: 250,
+                     child: Column(
+                       mainAxisAlignment: MainAxisAlignment.center,
+                       crossAxisAlignment: CrossAxisAlignment.start,
+                       children: [
+                         Text("공연명: " + shows_sort[index].show_name),
+                         SizedBox(height: 8),
+                         Text("출연진: " + shows_sort[index].actors.toString()),
+                       ],
+                     ),
                    ),
-                 )
-               ],
+                 ],
+               ),
              ),
            );
          }
